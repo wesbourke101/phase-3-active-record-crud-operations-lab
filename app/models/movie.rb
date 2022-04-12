92cd0d70eb34b80fb2648b54f22ccde29a1ea4b4
wesley <wesbourkeavl@gmail.com>
2022-04-12 14:03:37 -0400

    commit tree 3eac46f66e00cf1e47e4f358e4aa8fdd67eec626

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,49 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end    
 
+    def self.first_movie
+        Movie.first
+    end    
+
+    def self.last_movie
+        Movie.last
+    end
+    
+    def self.movie_count
+        Movie.count
+    end
+    
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+    
+    def self.find_movie_with_attributes(attributes)
+        Movie.find_by(attributes)
+    end    
+
+    def self.find_movies_after_2002
+        Movie.all.select do |movie|
+            movie.release_date > 2002
+        end
+    end   
+
+    def update_with_attributes(keyValue)
+        self.update(keyValue)
+    end  
+    
+    def self.update_all_titles(something)
+        Movie.update(title: something   )
+    end 
+    
+    def self.delete_by_id(id)
+        Movie.delete(id)
+    end
+    
+    def self.delete_all_movies
+        Movie.destroy_all
+    end    
+    
+    
 end
